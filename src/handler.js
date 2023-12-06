cecfa0193283676b9acc0c0b9bec62f577606616
Alif <m.alif.fadhillah32@gmail.com>
2023-12-06 14:44:28 +0800

    adding logic GET, POST, SET, and DELETE in handler.js

---
+++ src/handler.js
@@ -0,0 +1,20 @@
+const { nanoid } = require("nanoid");
+const books = require("./description");
+
+const addDescHandler = (request, h) => {};
+
+const getAllDescHandler = () => {};
+
+const getDescByIdHandler = (request, h) => {};
+
+const updateDescHandler = (request, h) => {};
+
+const deleteDescHandler = (request, h) => {};
+
+module.exports = {
+  addDescHandler,
+  getAllDescHandler,
+  getDescByIdHandler,
+  updateDescHandler,
+  deleteDescHandler,
+};
