2339dca669db52fc8d331de4134fef07fe7d4eff
Alif <m.alif.fadhillah32@gmail.com>
2023-12-06 23:46:13 +0800

    adding the addDescHandler on handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,59 @@
 const { nanoid } = require("nanoid");
 const books = require("./description");
 
-const addDescHandler = (request, h) => {};
+const addDescHandler = (request, h) => {
+  const {
+    name,
+    about,
+    soilMoisture,
+    recomFertilize1,
+    recomFertilize2,
+    recomFertilize3,
+    FertilizationRecommendations,
+    difficulty,
+    size,
+    type,
+    watering,
+  } = request.payload;
+
+  if (!name) {
+    return h
+      .response({
+        status: "fail",
+        message: "Failed to add plant. Please provide the plant name",
+      })
+      .code(400);
+  }
+
+  const id = nanoid();
+
+  const newDesc = {
+    id,
+    name,
+    about,
+    soilMoisture,
+    recomFertilize1,
+    recomFertilize2,
+    recomFertilize3,
+    FertilizationRecommendations,
+    difficulty,
+    size,
+    type,
+    watering,
+  };
+
+  description.push(newDesc);
+
+  return h
+    .response({
+      status: "success",
+      message: "The plant has been successfully added!",
+      data: {
+        DescId: id,
+      },
+    })
+    .code(201);
+};
 
 const getAllDescHandler = () => {};
 
